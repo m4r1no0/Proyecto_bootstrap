86bf500fba458af69f6e8d3fd2bb0e0e23a93389
m4r1no0 <marinoosorio97@gmail.com>
2025-02-11 20:09:44 -0500

    actualizacion cantidad

--- js/codigo.js
+++ js/codigo.js
@@ -14,65 +14,80 @@ botonBand.forEach(function(boton) {
     boton.addEventListener("click", function() {
     let botonNumero = boton;
     const precio = document.querySelector('.precioUnit');
+    const contador = document.querySelector('.contador');
+    digito = 1; 
+    contador.textContent = digito;
     if(boton.id == "botonUno"){
         const imagenUno = document.querySelector('.imgenContenedor');
         const precio = document.querySelector('.precioUnit');
         imagenUno.classList.add("imagenUno");
         precio.textContent=precios[0];
-        check.forEach(function(evento) {
-            evento.addEventListener("change", function() {
-                let  eventos = evento;
-                if(eventos.id == "flexCheckChecked1"){
-                    let total = precios[0]*1
-                    precio.textContent=total;
-                }else if(eventos.id == "flexCheckChecked2"){
-                    total = precios[0]*2
-                    precio.textContent=total;
+        const evento = document.querySelectorAll('.Checked');
+        evento.forEach(function(evento){
+            evento.addEventListener('click',function(){
+                if(evento.id == "suma"){
+                    digito++;
+                    contador.textContent = digito;
+                    total =precios[0]*digito;
+                    precio.textContent = total;
+                }else if (evento.id == "resta"){
+                    if(digito >= 1){
+                    digito--;
+                    total = precios[0]*digito;
+                    contador.textContent = digito;
+                    precio.textContent = total;
+                    }
                 }else{
-                    total = precios[0]*3
-                    precio.textContent=total;
+                    alert('No se puede realizar esa acción');
                 };
-                console.log(total);
             });
         });
     }else if(boton.id == "botonDos"){
         const imagenDos = document.querySelector('.imgenContenedor');
         imagenDos.classList.add("imagenDos");
         precio.textContent=precios[1];
-        check.forEach(function(evento) {
-            evento.addEventListener("change", function() {
-                let  eventos = evento;
-                if(eventos.id == "flexCheckChecked1"){
-                    let total = precios[1]*1
-                    precio.textContent=total;
-                }else if(eventos.id == "flexCheckChecked2"){
-                    total = precios[1]*2
-                    precio.textContent=total;
+        const evento = document.querySelectorAll('.Checked');
+        evento.forEach(function(evento){
+            evento.addEventListener('click',function(){
+                if(evento.id == "suma"){
+                    digito++;
+                    contador.textContent = digito;
+                    total =precios[1]*digito;
+                    precio.textContent = total;
+                }else if (evento.id == "resta"){
+                    if(digito >= 1){
+                    digito--;
+                    total = precios[0]*digito;
+                    contador.textContent = digito;
+                    precio.textContent = total;
+                    }
                 }else{
-                    total = precios[1]*3
-                    precio.textContent=total;
+                    alert('No se puede realizar esa acción');
                 };
-                console.log(total);
             });
         });
     }else if(boton.id == "botonTres"){
         const imagenTres = document.querySelector('.imgenContenedor');
         imagenTres.classList.add("imagenTres");
         precio.textContent=precios[2];
-        check.forEach(function(evento) {
-            evento.addEventListener("change", function() {
-                let  eventos = evento;
-                if(eventos.id == "flexCheckChecked1"){
-                    let total = precios[2]*1
-                    precio.textContent=total;
-                }else if(eventos.id == "flexCheckChecked2"){
-                    total = precios[2]*2
-                    precio.textContent=total;
+        const evento = document.querySelectorAll('.Checked');
+        evento.forEach(function(evento){
+            evento.addEventListener('click',function(){
+                if(evento.id == "suma"){
+                    digito++;
+                    contador.textContent = digito;
+                    total =precios[2]*digito;
+                    precio.textContent = total;
+                }else if (evento.id == "resta"){
+                    if(digito >= 1){
+                    digito--;
+                    total = precios[0]*digito;
+                    contador.textContent = digito;
+                    precio.textContent = total;
+                    }
                 }else{
-                    total = precios[2]*3
-                    precio.textContent=total;
+                    alert('No se puede realizar esa acción');
                 };
-                console.log(total);
             });
         });
     }else if(boton.id == "botonCuatro"){
@@ -80,60 +95,72 @@ botonBand.forEach(function(boton) {
         const precio = document.querySelector('.precioUnit');
         imagenCuatro.classList.add("imagenCuatro");
         precio.textContent=precios[3];
-        check.forEach(function(evento) {
-            evento.addEventListener("change", function() {
-                let  eventos = evento;
-                if(eventos.id == "flexCheckChecked1"){
-                    let total = precios[3]*1
-                    precio.textContent=total;
-                }else if(eventos.id == "flexCheckChecked2"){
-                    total = precios[3]*2
-                    precio.textContent=total;
+        const evento = document.querySelectorAll('.Checked');
+        evento.forEach(function(evento){
+            evento.addEventListener('click',function(){
+                if(evento.id == "suma"){
+                    digito++;
+                    contador.textContent = digito;
+                    total =precios[3]*digito;
+                    precio.textContent = total;
+                }else if (evento.id == "resta"){
+                    if(digito >= 1){
+                    digito--;
+                    total = precios[0]*digito;
+                    contador.textContent = digito;
+                    precio.textContent = total;
+                    }
                 }else{
-                    total = precios[3]*3
-                    precio.textContent=total;
+                    alert('No se puede realizar esa acción');
                 };
-                console.log(total);
             });
         });
     }else if(boton.id == "botonCinco"){
         const imagenCinco = document.querySelector('.imgenContenedor');
         imagenCinco.classList.add("imagenCinco");
         precio.textContent=precios[4];
-        check.forEach(function(evento) {
-            evento.addEventListener("change", function() {
-                let  eventos = evento;
-                if(eventos.id == "flexCheckChecked1"){
-                    let total = precios[4]*1
-                    precio.textContent=total;
-                }else if(eventos.id == "flexCheckChecked2"){
-                    total = precios[4]*2
-                    precio.textContent=total;
+        const evento = document.querySelectorAll('.Checked');
+        evento.forEach(function(evento){
+            evento.addEventListener('click',function(){
+                if(evento.id == "suma"){
+                    digito++;
+                    contador.textContent = digito;
+                    total =precios[4]*digito;
+                    precio.textContent = total;
+                }else if (evento.id == "resta"){
+                    if(digito >= 1){
+                    digito--;
+                    total = precios[0]*digito;
+                    contador.textContent = digito;
+                    precio.textContent = total;
+                    }
                 }else{
-                    total = precios[4]*3
-                    precio.textContent=total;
+                    alert('No se puede realizar esa acción');
                 };
-                console.log(total);
             });
         });
     }else{
         const imagenSeis = document.querySelector('.imgenContenedor');
         imagenSeis.classList.add("imagenSeis");
         precio.textContent=precios[5];
-        check.forEach(function(evento) {
-            evento.addEventListener("change", function() {
-                let  eventos = evento;
-                if(eventos.id == "flexCheckChecked1"){
-                    let total = precios[5]*1
-                    precio.textContent=total;
-                }else if(eventos.id == "flexCheckChecked2"){
-                    total = precios[5]*2
-                    precio.textContent=total;
+        const evento = document.querySelectorAll('.Checked');
+        evento.forEach(function(evento){
+            evento.addEventListener('click',function(){
+                if(evento.id == "suma"){
+                    digito++;
+                    contador.textContent = digito;
+                    total =precios[5]*digito;
+                    precio.textContent = total;
+                }else if (evento.id == "resta"){
+                    if(digito >= 1){
+                    digito--;
+                    total = precios[0]*digito;
+                    contador.textContent = digito;
+                    precio.textContent = total;
+                    }
                 }else{
-                    total = precios[5]*3
-                    precio.textContent=total;
+                    alert('No se puede realizar esa acción');
                 };
-                console.log(total);
             });
         });
     };
@@ -141,22 +168,4 @@ botonBand.forEach(function(boton) {
     carrito.classList.remove("d-none");
     });
 });
-
-    
-    check.forEach(function(evento) {
-        evento.addEventListener("change", function() {
-            let  eventos = evento;
-            if(eventos.id == "flexCheckChecked1"){
-                let total = precios[0]*1
-                precio.textContent=total;
-            }else if(eventos.id == "flexCheckChecked2"){
-                total = precios[0]*2
-                precio.textContent=total;
-            }else{
-                total = precios[0]*3
-                precio.textContent=total;
-            };
-            console.log(total);
-        });
-    });
     
